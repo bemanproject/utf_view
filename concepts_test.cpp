6d24e32b6b66fbe5a883dc4673d5a1aeb15587a8
Edward Nolan <eddiejnolan@gmail.com>
2024-01-14 19:06:56 -0500

    fix reference type

--- concepts_test.cpp
+++ concepts_test.cpp
@@ -20,27 +20,27 @@ namespace p2728 {
 #endif
   static_assert(!code_unit<int>);
 
-  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<char8_t, char8_t&,
+  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<char8_t, char8_t const&,
                                                                        char8_t>>);
-  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<char16_t, char16_t&,
+  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<char16_t, char16_t const&,
                                                                        char16_t>>);
-  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<char32_t, char32_t&,
+  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<char32_t, char32_t const&,
                                                                        char32_t>>);
 #ifdef WCHAR_T_IS_CODE_UNIT
-  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<wchar_t, wchar_t&,
+  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<wchar_t, wchar_t const&,
                                                                        wchar_t>>);
 #ifdef CHAR_IS_CODE_UNIT
-  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<char, char&,
+  static_assert(utf_iter<std_archetypes::basic_input_iterator_archetype<char, char const&,
                                                                        char>>);
 #else
-  static_assert(!utf_iter<std_archetypes::basic_input_iterator_archetype<char, char&,
+  static_assert(!utf_iter<std_archetypes::basic_input_iterator_archetype<char, char const&,
                                                                         char>>);
 #endif
 #else
-  static_assert(!utf_iter<std_archetypes::basic_input_iterator_archetype<wchar_t, wchar_t&,
+  static_assert(!utf_iter<std_archetypes::basic_input_iterator_archetype<wchar_t, wchar_t const&,
                                                                        wchar_t>>);
 #endif
-  static_assert(!utf_iter<std_archetypes::basic_input_iterator_archetype<int, int&, int>>);
+  static_assert(!utf_iter<std_archetypes::basic_input_iterator_archetype<int, int const&, int>>);
 
 #ifdef CHARN_T_POINTERS_ARE_RANGE_LIKE
   static_assert(utf_pointer<char8_t*>);
