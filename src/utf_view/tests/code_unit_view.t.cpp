8718c2324b68751cd3e02980e586c4324539919c
Edward Nolan <eddiejnolan@gmail.com>
2024-11-13 13:08:15 -0500

    Add static assertions for iterator concept satisfaction of code unit view iterator

--- src/utf_view/tests/code_unit_view.t.cpp
+++ src/utf_view/tests/code_unit_view.t.cpp
@@ -10,8 +10,60 @@
 #include <ranges>
 #include <string_view>
 #include <tests/framework.hpp>
+#include <tests/test_iterators.hpp>
 
 namespace utf_view::tests {
+
+static_assert(
+  std::input_iterator<
+    std::ranges::iterator_t<
+      as_char8_t_view<
+        std::ranges::subrange<test_input_iterator<char>, std::default_sentinel_t>>>>);
+
+static_assert(
+  std::input_iterator<
+    std::ranges::iterator_t<
+      as_char8_t_view<
+        std::ranges::subrange<test_comparable_input_iterator<char>,
+                              std::default_sentinel_t>>>>);
+
+static_assert(
+  std::input_iterator<
+    std::ranges::iterator_t<
+      as_char8_t_view<
+        std::ranges::subrange<test_copyable_input_iterator<char>,
+                              std::default_sentinel_t>>>>);
+static_assert(
+  !std::forward_iterator<
+    std::ranges::iterator_t<
+      as_char8_t_view<
+        std::ranges::subrange<test_copyable_input_iterator<char>,
+                              std::default_sentinel_t>>>>);
+
+static_assert(
+  std::forward_iterator<
+    std::ranges::iterator_t<
+      as_char8_t_view<
+        std::ranges::subrange<test_forward_iterator<char>,
+                              std::default_sentinel_t>>>>);
+static_assert(
+  std::forward_iterator<
+    std::ranges::sentinel_t<
+      as_char8_t_view<
+        std::ranges::subrange<test_forward_iterator<char>,
+                              test_forward_iterator<char>>>>>);
+static_assert(
+  std::bidirectional_iterator<
+    std::ranges::iterator_t<
+      as_char8_t_view<
+        std::ranges::subrange<test_bidi_iterator<char>,
+                              std::default_sentinel_t>>>>);
+static_assert(
+  std::bidirectional_iterator<
+    std::ranges::sentinel_t<
+      as_char8_t_view<
+        std::ranges::subrange<test_bidi_iterator<char>,
+                              test_bidi_iterator<char>>>>>);
 
 // TODO: Comprehensive testing for `code_unit_view`
 
