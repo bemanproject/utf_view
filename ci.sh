484d1ea921a52be82f1089bbddb48ba970fa5df2
Edward Nolan <eddiejnolan@gmail.com>
2024-11-13 15:22:55 -0500

    Add CMake option controlling whether tests are built
    This commit eliminates our use of BUILD_TESTING and adds a
    project-specific UTF_VIEW_BUILD_TESTING CMake option. It avoids
    including CTest and relies on calls to enable_testing(), which is what
    Beman repositories do to ensure sufficient granularity.

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ function main() {
     local checkout="$PWD"
     mkdir "$checkout/build"
     cd "$checkout/build"
-    cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=23 -DBUILD_TESTING=On "$@"
+    cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=23 -DUTF_VIEW_BUILD_TESTING=On "$@"
     cmake --build . --parallel
     export ASAN_OPTIONS=alloc_dealloc_mismatch=0 # https://github.com/llvm/llvm-project/issues/59432
     ctest -C Debug
